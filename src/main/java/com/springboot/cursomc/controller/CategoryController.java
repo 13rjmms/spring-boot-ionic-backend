e81891461ad43055692b7b0a05e1eba8484d2d95
Rafael Moreira <13rjmms@gmail.com>
2020-10-21 07:06:21 +0100

    Testing first entity class

--- src/main/java/com/springboot/cursomc/controller/CategoryController.java
+++ src/main/java/com/springboot/cursomc/controller/CategoryController.java
@@ -1,8 +1,13 @@
 package com.springboot.cursomc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.cursomc.entity.Category;
 
 @RestController
 @RequestMapping("/categories")
@@ -10,8 +15,16 @@ public class CategoryController {
 	
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String list() {
-		return "rest ok";
+	public List<Category> list() {
+		
+		Category cat1 = new Category(1, "Computing");
+		Category cat2 = new Category(2, "Office");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 	
 	
