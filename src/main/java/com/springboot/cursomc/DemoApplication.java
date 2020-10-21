7d194d8c5ef641e514c9e93a0d490e27a4ce2f41
Rafael Moreira <13rjmms@gmail.com>
2020-10-21 08:03:26 +0100

    creating instantiation operation

--- src/main/java/com/springboot/cursomc/DemoApplication.java
+++ src/main/java/com/springboot/cursomc/DemoApplication.java
@@ -1,13 +1,33 @@
 package com.springboot.cursomc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.springboot.cursomc.dao.CategoryDAO;
+import com.springboot.cursomc.entity.Category;
+
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoryDAO categoryDAO;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(1, "Computing");
+		Category cat2 = new Category(2, "Office");
+		
+		categoryDAO.saveAll(Arrays.asList(cat1, cat2)); 
 	}
 
 }
