d0d88a3e64d4bed31d6763a70dbbf1c08c62e194
Rafael Moreira <13rjmms@gmail.com>
2020-10-29 12:39:06 +0000

    Listing all categories using DTO (Data Transfer Object)

--- src/main/java/com/springboot/cursomc/controllers/CategoryController.java
+++ src/main/java/com/springboot/cursomc/controllers/CategoryController.java
@@ -1,7 +1,8 @@
 package com.springboot.cursomc.controllers;
 
-
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -13,10 +14,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.springboot.cursomc.dto.CategoryDTO;
 import com.springboot.cursomc.entitys.Category;
 import com.springboot.cursomc.service.exceptions.DataIntegrityException;
 import com.springboot.cursomc.services.CategoryService;
@@ -32,47 +33,48 @@ public class CategoryController {
 	public ResponseEntity<Category> find(@PathVariable Integer id) {
 
 		Category obj = categoryService.findById(id);
-		
 
 		return ResponseEntity.ok().body(obj);
 	}
-	
-	
+
 	@PostMapping()
-	public ResponseEntity<Void> insert(@RequestBody Category obj){
+	public ResponseEntity<Void> insert(@RequestBody Category obj) {
 		obj = categoryService.insert(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-				.path("/{id}").buildAndExpand(obj.getId()).toUri();
-		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
 		return ResponseEntity.created(uri).build();
-		
+
 	}
-	
-	
+
 	@PutMapping("/{id}")
-	public ResponseEntity<Void> insert(@RequestBody Category obj, @PathVariable Integer id){
+	public ResponseEntity<Void> insert(@RequestBody Category obj, @PathVariable Integer id) {
 		obj.setId(id);
 		obj = categoryService.update(obj);
-		
+
 		return ResponseEntity.noContent().build();
-		
+
 	}
-	
+
 	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> delete(@PathVariable Integer id){
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		try {
 			categoryService.deleteById(id);
-			
+
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("It is not possible to exclude a category that contains products");
-			
-		}
-		
-		
-		return ResponseEntity.noContent().build();
-		
-	}
-	
-	
 
+		}
+
+		return ResponseEntity.noContent().build();
+
+	}
+
+	@GetMapping
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+
+		List<Category> list = categoryService.findAll();
+		List<CategoryDTO> listDTO = list.stream().map(obj -> new CategoryDTO(obj)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listDTO);
+	}
 }
--- src/main/java/com/springboot/cursomc/services/CategoryService.java
+++ src/main/java/com/springboot/cursomc/services/CategoryService.java
@@ -1,5 +1,6 @@
 package com.springboot.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,21 +22,23 @@ public class CategoryService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Category.class.getName()));
 	}
-	
-	
+
 	public Category insert(Category obj) {
 		obj.setId(null);
 		return categoryDAO.save(obj);
 	}
-	
+
 	public Category update(Category obj) {
 		findById(obj.getId());
 		return categoryDAO.save(obj);
 	}
-	
+
 	public void deleteById(Integer id) {
 		categoryDAO.deleteById(id);
-		
+
 	}
-	
+
+	public List<Category> findAll() {
+		return categoryDAO.findAll();
+	}
 }
